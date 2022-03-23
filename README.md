ba2f3605748894a434186e1b3692ed4ddc1df819
emsnascimento <emilly.vcnascimento@gmail.com>
2022-03-23 09:37:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-ahoymb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ahoymb)
---
+++ script.js
@@ -0,0 +1,46 @@
+function sorteiaNumeroInteiro(n) {
+  return parseInt(Math.random() * n);
+}
+
+function limparFocarInput(idInput) {
+  document.getElementById(idInput).value = '';
+  document.getElementById(idInput).focus();
+}
+
+var numeroSecreto = sorteiaNumeroInteiro(11);
+console.log(numeroSecreto);
+var tentativas = 3;
+limparFocarInput('inputBox');
+
+function chutar() {
+  var elementoResultado = document.getElementById('resultado');
+  var elementoTentativa = document.getElementById('tentativa');
+  var btnTenteNovamente = document.getElementById('botaoTenteNovamente');
+  var chute = parseInt(document.getElementById('inputBox').value);
+
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = `Você acertou! Eu realmente estava pensando no número ${numeroSecreto}`;
+    document.getElementById('chutar').style.visibility = 'hidden';
+    btnTenteNovamente.removeAttribute('hidden');
+    tentativas = tentativas - 1;
+    elementoTentativa.innerHTML = `Tentativas: ${tentativas}`;
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = 'Digite um número entre 0 e 10!';
+    limparFocarInput('inputBox');
+  } else if (chute > numeroSecreto) {
+    elementoResultado.innerHTML = 'Errou, estou pensando em um número menor';
+    tentativas = tentativas - 1;
+    elementoTentativa.innerHTML = `Tentativas: ${tentativas}`;
+    limparFocarInput('inputBox');
+  } else if (chute < numeroSecreto) {
+    elementoResultado.innerHTML = 'Errou, estou pensando em um número maior';
+    tentativas = tentativas - 1;
+    elementoTentativa.innerHTML = `Tentativas: ${tentativas}`;
+    limparFocarInput('inputBox');
+  }
+  if (tentativas == 0) {
+    document.getElementById('chutar').style.visibility = 'hidden';
+    elementoResultado.innerHTML = `Você perdeu, o numero secreto era ${numeroSecreto}`;
+    btnTenteNovamente.removeAttribute('hidden');
+  }
+}
